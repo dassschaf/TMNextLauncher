66cc06dea8ccc6856cd898b2e4d9168c16435f7b
dasschaf <timmy@fruchtwein.net>
2021-01-10 21:43:50 +0100

    Projektdateien hinzufügen.

---
+++ GameSettings/DisplaySettings.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameSettings
+{
+    class DisplaySettings
+    {
+
+        string DisplayMode;
+
+        string ScreenSizeFS;
+
+        string ScreenSizeWin;
+
+        uint RefreshRate;
+
+        string DisplaySync;
+
+        string TripleBuffer;
+
+        bool Customize = true;
+
+        string Preset = "none";
+
+        string Antialiasing;
+
+        string DeferredAA;
+
+        ShaderQuality ShaderQuality;
+
+        TexturesQuality TexturesQuality;
+
+        string FilterAnisoQ;
+
+        string ZClip;
+
+        string ZClipAuto;
+
+        uint ZClipBlock;
+
+        GeometryQuality GeometryQuality;
+
+    }
+}
---
+++ GameSettings/Enums.cs
@@ -0,0 +1,18 @@
+﻿namespace GameSettings
+{
+
+    enum ShaderQuality
+    {
+        very_fast
+    }
+    
+    enum TexturesQuality
+    {
+        very_low
+    }
+
+    enum GeometryQuality
+    {
+        very_fast
+    }
+}
