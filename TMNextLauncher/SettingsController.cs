52d33fa2bf2a5748bcb699f425fc8cd6f8f2ce96
dassschaf <timmy@fruchtwein.net>
2021-01-14 13:14:01 +0100

    fluff

---
+++ TMNextLauncher/SettingsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace TMNextLauncher
+{
+    class SettingsController
+    {
+        public static GameSettings.GameSettings settings;
+
+        public SettingsController()
+        {
+            
+        }
+
+        static async void ReadGameSettings()
+        {
+        
+        }
+
+        static async void WriteGameSettings()
+        {
+        
+        }
+    }
+}
