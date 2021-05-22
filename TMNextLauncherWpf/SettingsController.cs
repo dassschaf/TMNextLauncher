7214ffd40884ed2ea4215dce435ced4bdf9c467f
Thomas <timmy@fruchtwein.net>
2021-05-22 14:44:08 +0200

    load and save settings

--- TMNextLauncherWpf/SettingsController.cs
+++ TMNextLauncherWpf/SettingsController.cs
@@ -1,20 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Windows;
+using System.IO;
 
 namespace TMNextLauncherWpf
 {
-    class SettingsController
+    public class SettingsController
     {
-        public GameSettings.GameSettings Settings;
+        public GameSettings.GameSettings settings;
 
-        void ReadSettingsFromFile()
+        public void ReadSettingsFromFile()
         {
+            if (Properties.Settings.Default.SettingsJsonPath == "")
+            {
+                MessageBox.Show("No settings file had been set yet.");
+                return;
+            }
+
+            // grab json
+            string json = File.ReadAllText(Properties.Settings.Default.SettingsJsonPath);
+
+            // parse
+            this.settings = GameSettings.GameSettings.SettingsFromJson(json);            
         }
 
-        void SaveSettingsToFile()
+        public void SaveSettingsToFile()
         {
+            if (Properties.Settings.Default.SettingsJsonPath == "")
+            {
+                MessageBox.Show("No settings file had been set yet.");
+                return;
+            }
+
+            if (this.settings == null)
+            {
+                MessageBox.Show("There's nothing to save yet.");
+                return;
+            }
+
+            // get json
+            string json = settings.JsonExport();
+
+            // save
+            File.WriteAllText(Properties.Settings.Default.SettingsJsonPath, json);
         }
 
     }
